161dd632324241e8b9fd0a1ed02710b16cf0e3af
Hiromaru8 <135395020+hiromaru8@users.noreply.github.com>
2023-06-27 06:05:10 +0900

    Update convert_csv_to_text_w_ruledLine.py

--- convert2textfile/convert_csv_to_text_w_ruledLine.py
+++ convert2textfile/convert_csv_to_text_w_ruledLine.py
@@ -4,17 +4,21 @@ def convert_csv_to_text_w_ruledLine(csv_file):
     # CSVファイルを読み込む
     df = pd.read_csv(csv_file)
 
+    # 各列の最大幅を取得
+    col_widths = [df[col].astype(str).str.len().max() for col in df.columns]
+
     # 表をテキスト形式に変換する
     text = ""
     for _, row in df.iterrows():
-        for value in row:
-            text += str(value) + "│"
-        text += "\n━━━" * len(row) + "\n"
+        for i, value in enumerate(row):
+            text += str(value).ljust(col_widths[i]) + "|"
+        text += "\n"
+        #text += "-" * (sum(col_widths) + len(col_widths) - 1) + "\n"
 
     return text
 
 # CSVファイルのパスを指定して変換する
-csv_file = "path/to/your/csv/file.csv"
+csv_file = "D:\\download\\file.csv"
 converted_text = convert_csv_to_text_w_ruledLine(csv_file)
 
 # 変換結果を表示する
